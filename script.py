7a6efd7d095276138ca376b10696796c6ff3edf0
HeyThisWasRandomlyMade <84421621+HeyThisWasRandomlyMade@users.noreply.github.com>
2024-06-02 14:12:53 -0600

    Update script.py

--- script.py
+++ script.py
@@ -7,13 +7,11 @@ def	custom_generate_chat_prompt(user_input, state, **kwargs):
 	This extension will replace all instances of
 	{{time}} and {{date}} with the current
 	time and date, respectively
-	Additionally replaces {{weekday}} with the day of the week.
+	Additionall replaces {{weekday}} with the day of the week.
 	'''
-	now = datetime.now()
-	state['context'] = state['context'].replace('{{time}}', now.strftime("%I:%M %p"))
-    	state['context'] = state['context'].replace('{{date}}', now.strftime("%Y %B, %d"))
-    	state['context'] = state['context'].replace('{{weekday}}', now.strftime("%A"))
-	state['context'] = state['context'].replace('{{timezone}}', now.strftime("%Z"))
+	state['context'] = state['context'].replace('{{time}}',datetime.now().strftime("%I:%M %p")) 
+	state['context'] = state['context'].replace('{{date}}',datetime.now().strftime("%B %d, %Y"))
+	state['context'] = state['context'].replace('{{weekday}}',("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")[datetime.now().weekday()])
 	# print(f'<<context, changed>>')
 	# print(f'{state["context"]}')
-	return 
+	return
